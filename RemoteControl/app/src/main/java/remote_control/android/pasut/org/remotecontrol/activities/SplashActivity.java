a0dcc32a05e98185af3a9cd1fb0a10c3a29a5bb9
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-07-04 19:53:41 -0300

    trabajo en first running experience

--- RemoteControl/app/src/main/java/remote_control/android/pasut/org/remotecontrol/activities/SplashActivity.java
+++ RemoteControl/app/src/main/java/remote_control/android/pasut/org/remotecontrol/activities/SplashActivity.java
@@ -1,32 +1,46 @@
 package remote_control.android.pasut.org.remotecontrol.activities;
 
-import remote_control.android.pasut.org.remotecontrol.activities.util.SystemUiHider;
-
-import android.annotation.TargetApi;
-import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 
+import com.google.inject.Inject;
+
 import remote_control.android.pasut.org.remotecontrol.R;
+import remote_control.android.pasut.org.remotecontrol.services.PreferencesService;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
 
+@ContentView(R.layout.activity_splash)
+public class SplashActivity extends RoboActivity {
+    private final static String FRE = "FIRST_RUNNING_EXPERIENCE";
+    private final static int RESPONSE = 1;
 
-public class SplashActivity extends Activity {
+    @Inject
+    private PreferencesService preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //this.getActionBar().hide();
-
-        setContentView(R.layout.activity_splash);
         ImageView loadingImage = (ImageView) findViewById(R.id.loading);
 
         AnimationDrawable loadingAnimation = (AnimationDrawable) loadingImage.getBackground();
         loadingAnimation.start();
+
+        if (!preferences.contain(FRE)) {
+            startActivityForResult(new Intent(this, SettingsActivity.class), RESPONSE);
+            preferences.put(FRE, true);
+        } else {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
